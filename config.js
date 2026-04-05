fd50d85d46891139cc0c435436af4121b743d36b
cihan-ozcan <115652471+cihan-ozcan@users.noreply.github.com>
2026-04-05 12:00:37 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,13 +7,3 @@ window.FILO_CONFIG = {
   SUPABASE_URL  : 'https://fjetoktgzpubegpvhhng.supabase.co',
   SUPABASE_ANON : 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZqZXRva3RnenB1YmVncHZoaG5nIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUxNTYwMjMsImV4cCI6MjA5MDczMjAyM30.WSTtAwD9vtm4fgJsa6K4DyLHFD4iUyGuF6qkR-0Uop0',
 };
-
-let currentFirmaId = null;
-
-async function loadFirmaId() {
-  const { data } = await _sb
-    .from('firma_kullanicilar')
-    .select('firma_id')
-    .single();
-  currentFirmaId = data?.firma_id || null;
-}
